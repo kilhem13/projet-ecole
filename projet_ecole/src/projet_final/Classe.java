6bbedfc1f767cefff97a738e15b0f5532f651a47
loulouchoco64 <36404423+loulouchoco64@users.noreply.github.com>
2019-06-06 11:41:49 +0200

    reparer guilhem betise

---
+++ projet_ecole/src/projet_final/Classe.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projet_ecole;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author sarto
+ */
+public class Classe {
+    private String nom;
+    private String niveau;
+    private ArrayList<Personne> eleve_liste;
+    private int id;
+
+}
---
+++ projet_ecole/src/projet_final/Date.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projet_ecole;
+
+/**
+ *
+ * @author sarto
+ */
+public class Date {
+    private int jour;
+    private int mois;
+    private int annee;
+
+}
